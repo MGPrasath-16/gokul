a89b307203f01a383848bc9182405b6bd96bad49
root <root@ip-172-31-1-83.ap-south-1.compute.internal>
2023-03-30 15:39:09 +0000

    first commit

---
+++ new.py
@@ -0,0 +1,32 @@
+
+"""print("enter a number")
+a=int(input())
+print("enter another number")
+b=int(input())
+print("enter the third number")
+c=int(input())
+
+if a>b:
+    if a>c:
+        print("The biggest value is",a)
+    else:
+        print("the biggest value is",c)
+#elif b>c:print("The Two value are equal",a,b)
+else:
+    if b>c:
+        print("The biggest value is", b)
+    else:
+        print("the biggest value is",c)"""
+        
+a=int(99)
+#print(a,type(a))
+while (a>0):
+    print(a)
+    a-=2
+"""print("type yes for ascending order")
+c=str(input())
+d=str("yes")
+if (c==d):
+    while(a<99):
+        a+=2
+        print(a)"""
